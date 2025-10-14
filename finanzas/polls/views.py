cd130b82e800aec209a2201439ddc3226fa143f5
1MT3DDY <thebenja1222@gmail.com>
2025-10-14 17:18:29 -0300

    sigo
    el diablo anda suelto
    va caminando por el mismo pavimento

--- finanzas/polls/views.py
+++ finanzas/polls/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Estoy en el index de los polls")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context,request))
+
 
 def detail(request, question_id):
     return HttpResponse("Estas viendo la pregunta %s." % question_id)
