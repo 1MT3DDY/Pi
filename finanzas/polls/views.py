cf7cbfad4375be14fa7badcf86eaf226e6c5f4f4
1MT3DDY <thebenja1222@gmail.com>
2025-10-15 13:26:55 -0300

    Ultimo

--- finanzas/polls/views.py
+++ finanzas/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from django.template import loader
 from .models import Question
 # Create your views here.
@@ -10,9 +11,12 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return HttpResponse(template.render(context,request))
 
-
 def detail(request, question_id):
-    return HttpResponse("Estas viendo la pregunta %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("La pregunta aun no existe")
+    return render(request, "polls/detail.html", {"question:question"})
 
 def results(request, question_id):
     response = "Estas viendo los resultados de la pregunta %s."
